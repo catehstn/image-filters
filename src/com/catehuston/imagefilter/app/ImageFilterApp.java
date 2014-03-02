ef48af4adb1afc0f105c9928e8735513486179ad
Cate Huston <catehuston@gmail.com>
2014-03-02 23:49:11 +0000

    adds key handling, file changes

--- src/com/catehuston/imagefilter/app/ImageFilterApp.java
+++ src/com/catehuston/imagefilter/app/ImageFilterApp.java
@@ -1,11 +1,6 @@
 package com.catehuston.imagefilter.app;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
-
-import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
 
 import processing.core.PApplet;
 import processing.core.PImage;
@@ -18,24 +13,63 @@ import com.catehuston.imagefilter.model.HSBColor;
 public class ImageFilterApp extends PApplet {
 	
 	PImage img;
+	String filePath;
+	
+	static final int rgbColorRange = 100;
+	static final int filterHeight = 2;
+	static final int filterIncrement = 5;
 	static final int hueRange = 320; 
 	static final int hueTolerance = 10;
+	static final int imageMax = 640;
+	static final int sideBarPadding = 10;
+	static final int sideBarWidth = rgbColorRange + 2 * sideBarPadding;
+	
+	int redFilter = 0;
+	int blueFilter = 0;
+	int greenFilter = 0;
 
 	public void setup() {
 		noLoop();
 		
 		// Set up the view.
-		size(640, 640);
+		size(imageMax + sideBarWidth, imageMax);
 		background(0);
 
-		//Choose the file.
-		selectInput("Select a file to process:", "fileSelected");
+		chooseFile();
 	}
 	
 	public void draw() {
+		background(0);
+		
+		colorMode(RGB, rgbColorRange);
+		stroke(rgbColorRange);
+		line(imageMax, 0, imageMax, imageMax);
+		
+		// Draw red line
+		int x = imageMax + sideBarPadding;
+		int y = 2 * sideBarPadding;
+		stroke(rgbColorRange, 0, 0);
+		line(x, y, x + rgbColorRange, y);
+		line(x + redFilter, y - filterHeight, x + redFilter, y + filterHeight);
+		
+		// Draw green line
+		y += 2 * sideBarPadding;
+		stroke(0, rgbColorRange, 0);
+		line(x, y, x + rgbColorRange, y);
+		line(x + greenFilter, y - filterHeight, x + greenFilter, y + filterHeight);
+		
+		// Draw blue line
+		y += 2 * sideBarPadding;
+		stroke(0, 0, rgbColorRange);
+		line(x, y, x + rgbColorRange, y);
+		line(x + blueFilter, y - filterHeight, x + blueFilter, y + filterHeight);
+		
+		// Draw image.
 		if (img != null) {
-			image(img, 0, 0, 640, 640);
+			print("draw image\n");
+			drawImage();
 		}
+		updatePixels();
 	}
 	
 	// Callback for selectInput(), has to be public to be found.
@@ -43,68 +77,10 @@ public class ImageFilterApp extends PApplet {
 		if (file == null) {
 			println("User hit cancel.");
 		} else {
-			img = loadImage(file.getAbsolutePath());
-			chooseFilter();
+			this.filePath = file.getAbsolutePath();
+			img = loadImage(filePath);
+			redraw();
 		}
-	}
-	
-	private void chooseFilter() {
-		JPopupMenu popupMenu = new JPopupMenu("Filters");
-		JMenuItem menuItem1 = new JMenuItem("Show Dominent Hue Filter");
-		menuItem1.addActionListener(new ActionListener() {
-			@Override public void actionPerformed(ActionEvent e) {
-				dominantHueFilter();
-				redraw();
-			}
-		});
-		popupMenu.add(menuItem1);
-		
-		JMenuItem menuItem2 = new JMenuItem("Hide Dominent Hue Filter");
-		menuItem2.addActionListener(new ActionListener() {
-			@Override public void actionPerformed(ActionEvent e) {
-				hideDominentHueFilter();
-				redraw();
-			}
-		});
-		popupMenu.add(menuItem2);
-		
-		JMenuItem menuItem3 = new JMenuItem("Red Filter");
-		menuItem3.addActionListener(new ActionListener() {
-			@Override public void actionPerformed(ActionEvent e) {
-				colorFilter(30, 0, 0);;
-				redraw();
-			}
-		});
-		popupMenu.add(menuItem3);
-		
-		JMenuItem menuItem4 = new JMenuItem("Blue Filter");
-		menuItem4.addActionListener(new ActionListener() {
-			@Override public void actionPerformed(ActionEvent e) {
-				colorFilter(0, 30, 0);;
-				redraw();
-			}
-		});
-		popupMenu.add(menuItem4);
-		
-		JMenuItem menuItem5 = new JMenuItem("Green Filter");
-		menuItem5.addActionListener(new ActionListener() {
-			@Override public void actionPerformed(ActionEvent e) {
-				colorFilter(0, 0, 30);;
-				redraw();
-			}
-		});
-		popupMenu.add(menuItem5);
-		
-		JMenuItem menuItem6 = new JMenuItem("Composite Filter");
-		menuItem6.addActionListener(new ActionListener() {
-			@Override public void actionPerformed(ActionEvent e) {
-				colorFilter(30, 30, 30);;
-				redraw();
-			}
-		});
-		popupMenu.add(menuItem6);
-		
-		popupMenu.show(this, 0, 0);
 	}
 	
 	private void dominantHueFilter() {
@@ -116,6 +92,7 @@ public class ImageFilterApp extends PApplet {
 	}
 	
 	private void processImageForHue(boolean showHue) {
+		img = loadImage(filePath);
 		colorMode(HSB, (hueRange - 1));
 		img.loadPixels();
 		int numberOfPixels = img.pixels.length;
@@ -131,6 +108,7 @@ public class ImageFilterApp extends PApplet {
 				img.pixels[i] = color(brightness);
 			}
 		}
+		img.updatePixels();
 	}
 	
 	private static boolean hueInRange(float hue, float lower, float upper) {
@@ -147,9 +125,22 @@ public class ImageFilterApp extends PApplet {
 			return hue > upper || hue < lower;
 		}
 	}
+	
+	private void drawImage() {
+		colorFilter(redFilter, blueFilter, greenFilter);
+		int imgWidth = imageMax;
+		int imgHeight = imageMax;
+		if (img.width > img.height) {
+			imgHeight = (imgHeight *  img.height) / img.width;
+		} else {
+			imgWidth = (imgWidth * img.width) / img.height;
+		}
+		imageMode(CENTER);
+		image(img, 320, 320, imgWidth, imgHeight);
+	}
 
 	private void colorFilter(int minRed, int minBlue, int minGreen) {
-		colorMode(RGB, 100);
+		colorMode(RGB, rgbColorRange);
 		img.loadPixels();
 		int numberOfPixels = img.pixels.length;
 		for (int i = 0; i < numberOfPixels; i++) {
@@ -165,5 +156,49 @@ public class ImageFilterApp extends PApplet {
 			
 			img.pixels[i] = color((float) red, (float) green, (float) blue, (float) alpha);
 		}
+	}
+	
+	public void keyPressed() {
+		 switch (key) {
+		 case 'r':
+			 redFilter+=filterIncrement;
+			 redFilter = min(redFilter, rgbColorRange);
+			 break;
+		 case 'e':
+			 redFilter-=filterIncrement;
+			 redFilter = max(redFilter, 0);
+			 break;
+		 case 'g':
+			 greenFilter+=filterIncrement;
+			 greenFilter = min(greenFilter, rgbColorRange);
+			 break;
+		 case 'f':
+			 greenFilter-=filterIncrement;
+			 greenFilter = max(greenFilter, 0);
+			 break;
+		 case 'b':
+			 blueFilter+=filterIncrement;
+			 blueFilter = min(blueFilter, rgbColorRange);
+			 break;
+		 case 'v':
+			 blueFilter-=filterIncrement;
+			 blueFilter = max(blueFilter, 0);
+			 break;
+		 case 'c':
+			 chooseFile();
+			 break;
+		 case 'h':
+			 hideDominentHueFilter();
+			 break;
+		 case 's':
+			 dominantHueFilter();
+			 break;
+		 }
+		 redraw();
+	}
+	
+	private void chooseFile() {
+		//Choose the file.
+		selectInput("Select a file to process:", "fileSelected");
 	}
 }
